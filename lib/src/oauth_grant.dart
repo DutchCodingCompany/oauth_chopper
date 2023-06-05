a8fc9e90b423f1cfa78132ac1312a42edbfd68d5
Bas de Vaan <basdevaan@dutchcodingcompany.com>
2023-06-05 13:45:36 +0200

    add Authorization code grant

--- lib/src/oauth_grant.dart
+++ lib/src/oauth_grant.dart
@@ -3,8 +3,7 @@ import 'package:oauth2/oauth2.dart' as oauth;
 abstract class OAuthGrant {
   const OAuthGrant();
 
-  Future<String> handle(
-      Uri authorizationEndpoint, String identifier, String secret);
+  Future<String> handle(Uri authorizationEndpoint, String identifier, String secret);
 }
 
 /// Obtains credentials using a [resource owner password grant](https://tools.ietf.org/html/rfc6749#section-1.3.3).
@@ -12,12 +11,10 @@ class ResourceOwnerPasswordGrant extends OAuthGrant {
   final String username;
   final String password;
 
-  const ResourceOwnerPasswordGrant(
-      {required this.username, required this.password});
+  const ResourceOwnerPasswordGrant({required this.username, required this.password});
 
   @override
-  Future<String> handle(
-      Uri authorizationEndpoint, String identifier, String secret) async {
+  Future<String> handle(Uri authorizationEndpoint, String identifier, String secret) async {
     final client = await oauth.resourceOwnerPasswordGrant(
       authorizationEndpoint,
       username,
@@ -34,8 +31,7 @@ class ClientCredentialsGrant extends OAuthGrant {
   const ClientCredentialsGrant();
 
   @override
-  Future<String> handle(
-      Uri authorizationEndpoint, String identifier, String secret) async {
+  Future<String> handle(Uri authorizationEndpoint, String identifier, String secret) async {
     final client = await oauth.clientCredentialsGrant(
       authorizationEndpoint,
       identifier,
@@ -45,4 +41,34 @@ class ClientCredentialsGrant extends OAuthGrant {
   }
 }
 
-//TODO: Add AuthorizationCodeGrant
+/// Obtains credentials using a [authorization code grant](https://tools.ietf.org/html/rfc6749#section-1.3.1).
+class AuthorizationCodeGrant extends OAuthGrant {
+  const AuthorizationCodeGrant(
+    this.tokenEndpoint,
+    this.scopes,
+    this.redirectUrl,
+    this.redirect,
+    this.listen,
+  );
+
+  final Uri tokenEndpoint;
+  final Uri redirectUrl;
+  final List<String> scopes;
+  final Future<void> Function(Uri authorizationUri) redirect;
+  final Future<Uri> Function(Uri redirectUri) listen;
+
+  @override
+  Future<String> handle(Uri authorizationEndpoint, String identifier, String secret) async {
+    final grant = oauth.AuthorizationCodeGrant(
+      identifier,
+      authorizationEndpoint,
+      tokenEndpoint,
+    );
+    var authorizationUrl = grant.getAuthorizationUrl(redirectUrl, scopes: scopes);
+    await redirect(authorizationUrl);
+    var responseUrl = await listen(redirectUrl);
+    oauth.Client client = await grant.handleAuthorizationResponse(responseUrl.queryParameters);
+
+    return client.credentials.toJson();
+  }
+}
